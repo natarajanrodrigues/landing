9e2e14ed25ac5807065445483db826c6391da652
Natarajan Rodrigues <natarajanrodrigues@gmail.com>
2020-12-10 00:22:03 -0300

    Module 2: GraphQL

--- config/database.js
+++ config/database.js
@@ -8,8 +8,8 @@ module.exports = ({ env }) => ({
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'strapi2'),
-        username: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi123'),
+        username: env('DATABASE_USERNAME', 'postgres'),
+        password: env('DATABASE_PASSWORD', '12345'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
